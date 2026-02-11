e2eab16a0d8428056ab7df9b3f8062b47f2ece0a
elcapobrr <pacolao94@gmail.com>
2026-02-11 12:53:59 +0100

    V3
    V3

--- config/database.js
+++ config/database.js
@@ -1,6 +1,20 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+console.log('--- DB DEBUG ---');
+console.log(`DB_HOST: "${process.env.DB_HOST}"`);
+console.log(`DB_USER: "${process.env.DB_USER}"`);
+console.log(`DB_NAME: "${process.env.DB_NAME}"`);
+console.log(`DB_PORT: "${process.env.DB_PORT}"`);
+if (process.env.DB_PASSWORD) {
+    console.log(`DB_PASSWORD length: ${process.env.DB_PASSWORD.length}`);
+    console.log(`DB_PASSWORD first char: "${process.env.DB_PASSWORD[0]}"`);
+    console.log(`DB_PASSWORD last char: "${process.env.DB_PASSWORD[process.env.DB_PASSWORD.length - 1]}"`);
+} else {
+    console.log('DB_PASSWORD is NOT SET');
+}
+console.log('----------------');
+
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
